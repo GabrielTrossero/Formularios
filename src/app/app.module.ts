622c7eda06be5b0148e7a2004af00212d305d4f8
Gabriel Trossero <gabi_trossero@hotmail.com>
2022-07-27 11:11:23 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TemplateComponent } from './template/template.component';
+import { ModelComponent } from './model/model.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TemplateComponent,
+    ModelComponent
   ],
   imports: [
     BrowserModule,
