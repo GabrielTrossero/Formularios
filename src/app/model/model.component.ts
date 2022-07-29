8e0b48169cb76ce7e0cb95bd3c67f9f5c4510c93
Gabriel Trossero <gabi_trossero@hotmail.com>
2022-07-29 10:47:20 -0300

    Model Driven - Validaciones simples

--- src/app/model/model.component.ts
+++ src/app/model/model.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-model',
@@ -12,13 +12,23 @@ export class ModelComponent implements OnInit {
 
   constructor() { 
     this.formulario = new FormGroup({
-      nombre: new FormControl(),
-      apellido: new FormControl(),
-      edad: new FormControl(),
-      dni: new FormControl(),
-      password: new FormControl(),
-      repite_password: new FormControl(),
-      email: new FormControl(),
+      nombre: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3)
+      ]),
+      apellido: new FormControl('',[
+        Validators.maxLength(10)
+      ]),
+      edad: new FormControl('22'), //Podemos poner un valor por default
+      dni: new FormControl(''),
+      password: new FormControl(''),
+      repite_password: new FormControl(''),
+      email: new FormControl('',[
+        //Validators.email  Esta es una validación muy báscia, por lo que utilizaremos una externa
+        //La siguiente expresión regular es sacada de https://regexlib.com/
+        //Debemos colocarla entre "/ /"
+        Validators.pattern(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/)
+      ]),
     });
   }
 
